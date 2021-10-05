297e0264d1dfbbb25031cfde36a35d10df5a5f57
Duy Nguyen <zuik@users.noreply.github.com>
2021-10-05 13:22:05 +0000

    Simple db models

--- alembic/env.py
+++ alembic/env.py
@@ -15,9 +15,9 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from jdict_py import models
+
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -63,9 +63,7 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- jdict_py/models.py
+++ jdict_py/models.py
@@ -3,12 +3,42 @@ models.py - SQLAlchemy models for the Jdict database
 
 We will follow roughly how JMdict is organized.
 """
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class Entry(Base):
     """
     A word entry
     """
-    pass
+
+    __tablename__ = "entries"
+
+    entry_id = Column(Integer, primary_key=True)
+    reading_el = Column(String)
+    kanji_el = Column(String)
+    sense = Column(String)
+
+    @staticmethod
+    def new_entry(reading_el: str, kanji_el: str, sense: str):
+        """
+        Create a new entry
+        """
+        return Entry(reading_el=reading_el, kanji_el=kanji_el, sense=sense)
+
+    def __repr__(self):
+        return f"<Entry(entry_id={self.entry_id}, reading_el={self.reading_el}, kanji_el={self.kanji_el}, sense={self.sense})>"
+
+    def to_dict(self):
+        """
+        Return a dictionary representation of this object
+        """
+        return {
+            "entry_id": self.entry_id,
+            "reading_el": self.reading_el,
+            "kanji_el": self.kanji_el,
+            "sense": self.sense,
+        }
